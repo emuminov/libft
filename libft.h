32d28bc7dd895a26613e0ca17ec05dd4adfe25b2
pdlmn <jseldardw@gmail.com>
2023-11-05 00:35:07 +0100

    refactor: moved <stdlib> to libft header

--- libft.h
+++ libft.h
@@ -1,5 +1,11 @@
+#include <stdlib.h>
+
+// BONUS
 typedef struct s_list
 {
 	void *content;
 	struct s_list *next;
 }	t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
