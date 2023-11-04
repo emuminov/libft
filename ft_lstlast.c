33c82add4b2f0a5672535424ccbbf3bf4c692dd6
pdlmn <jseldardw@gmail.com>
2023-11-05 00:57:36 +0100

    feat: added ft_lstlast function

---
+++ ft_lstlast.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+	t_list	*curr;
+	t_list	*next;
+
+	curr = lst;
+	next = lst->next;
+	while (next)
+	{
+		curr = curr->next;
+		next = curr->next;
+	}
+	return (curr);
+}
