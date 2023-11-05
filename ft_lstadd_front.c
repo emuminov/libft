90f56332c14cea60f6f1ee5d32a15867fb51d838
pdlmn <jseldardw@gmail.com>
2023-11-06 00:21:01 +0100

    feat: added guards to ft_lstadd_front function

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -1,7 +1,25 @@
 #include "libft.h"
 
+/* t_list	*end;
+
+if (!lst || !new)
+return ;
+if (!(*lst))
+{
+	*lst = new;
+	return ;
+}
+end = ft_lstlast(*lst);
+end->next = new; */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (!lst || new)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
 	new->next = *lst;
 	*lst = new;
 }
