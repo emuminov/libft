1b3a8bd16ac9e0a77acfefb9e6dfeb7a768f28ae
pdlmn <jseldardw@gmail.com>
2023-11-05 22:27:12 +0100

    refactor: tweaked ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -4,13 +4,16 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_head;
 	t_list	*new_tail;
+	t_list	*new_node;
 
 	new_head = ft_lstnew((*f)(lst->content));
-	new_tail = new_head->next;
+	new_tail = new_head;
+	new_node = 0;
 	while (lst->next)
 	{
-		new_tail = ft_lstnew((*f)(lst->next->content));
-		ft_lstadd_back(&new_head, new_tail);
+		new_node = ft_lstnew((*f)(lst->next->content));
+		ft_lstadd_back(&new_tail, new_node);
+		new_tail = new_node;
 		lst = lst->next;
 	}
 	return (new_head);
