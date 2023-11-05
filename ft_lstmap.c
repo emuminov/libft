779a704f1c7c5f7f64589f00e0321f2120c495df
pdlmn <jseldardw@gmail.com>
2023-11-05 23:26:07 +0100

    feat: added guards to ft_lstmap function and normalised it

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,19 +1,42 @@
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+static t_list	*ft_apply(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*node;
+	void	*node_content;
+
+	node_content = (*f)(lst->content);
+	node = ft_lstnew(node_content);
+	if (!node)
+	{
+		(*del)(node_content);
+		return (0);
+	}
+	return (node);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_head;
 	t_list	*new_tail;
-	t_list	*new_node;
+	t_list	*node;
 
+	if (!lst || !f || !del)
+		return (0);
 	new_head = ft_lstnew((*f)(lst->content));
+	if (!new_head)
+		return (0);
 	new_tail = new_head;
-	new_node = 0;
 	while (lst->next)
 	{
-		new_node = ft_lstnew((*f)(lst->next->content));
-		ft_lstadd_back(&new_tail, new_node);
-		new_tail = new_node;
+		node = ft_apply(lst->next, f, del);
+		if (!node)
+		{
+			ft_lstclear(&new_head, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_tail, node);
+		new_tail = node;
 		lst = lst->next;
 	}
 	return (new_head);
