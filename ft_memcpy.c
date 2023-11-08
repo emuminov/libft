89e957d3233d8950022a65c90544a7174cfb94f3
pdlmn <jseldardw@gmail.com>
2023-11-08 13:19:22 +0100

    refactor: tweaked a little bit ft_memcpy for consistency

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 00:07:01 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/08 02:18:23 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/08 13:19:00 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	if (!dest)
-		return (dest);
+		return (0);
 	i = 0;
 	while (i < n)
 	{
