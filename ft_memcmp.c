d4269b2f622a8c249a892d0244bdadd82e51f4c2
pdlmn <jseldardw@gmail.com>
2023-11-09 17:32:15 +0100

    feat: added ft_memcmp function

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 17:26:57 by emuminov          #+#    #+#             */
+/*   Updated: 2023/11/09 17:31:23 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((char *)s1)[i] > ((char *)s2)[i])
+			return (1);
+		if (((char *)s1)[i] < ((char *)s2)[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
