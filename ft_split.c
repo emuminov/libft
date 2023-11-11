87bb11c546857b86eb42611115f76fce43af98a1
pdlmn <jseldardw@gmail.com>
2023-11-11 02:15:24 +0100

    feat: added ft_split function

---
+++ ft_split.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 01:18:39 by emuminov          #+#    #+#             */
+/*   Updated: 2023/11/11 02:13:59 by emuminov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int ft_count_words(char const *s, char c)
+{
+	int		words;
+	short	is_new_word;
+	size_t	i;
+
+	words = 0;
+	is_new_word = 1;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			is_new_word = 1;
+		else if (s[i] != c && is_new_word)
+		{
+			is_new_word = 0;
+			words++;
+		}
+		i++;
+	}
+	return (words);
+}
+
+char **ft_split(char const *s, char c)
+{
+	char	**strs;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	strs = malloc(ft_count_words(s, c) * sizeof(char *));
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			j = 1;
+			while (s[i + j] && s[i + j] != c)
+				j++;
+			strs[k++] = ft_substr(s, i, j);
+			i += j;
+		}
+	}
+	return (strs);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 00:06:35 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/10 18:54:40 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/11 01:18:59 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 // BONUS
 typedef struct s_list
