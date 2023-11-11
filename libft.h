b19f21202877e4eee419435631ddab5a9d1bad2c
pdlmn <jseldardw@gmail.com>
2023-11-11 20:59:36 +0100

    feat: added ft_striteri function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 00:06:35 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/11 20:51:10 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/11 20:57:22 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 // BONUS
 typedef struct s_list
