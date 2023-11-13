a8dcfe0ee913728a9604683bc55e68e82606bba4
pdlmn <jseldardw@gmail.com>
2023-11-13 10:49:53 +0100

    feat: added skup for whitespace/tab characters and added checks for plus sign in ft_atoi function

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 17:50:50 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/13 01:38:09 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/13 10:48:12 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,21 @@ int	ft_atoi(const char *nptr)
 	sign = 1;
 	result = 0;
 	i = 0;
+	while (nptr[i] == '\t' || nptr[i] == ' ' || nptr[i] == '\r'
+		|| nptr[i] == '\v' || nptr[i] == '\n' || nptr[i] == '\f')
+		i++;
 	if (nptr[i] == '-')
 	{
 		sign = -sign;
 		i++;
 	}
+	else if (nptr[i] == '+')
+		i++;
 	while (nptr[i])
 	{
 		if (!ft_isdigit(nptr[i]))
 			break ;
-		result *= 10;
-		result += nptr[i] - 48;
-		i++;
+		result = (result * 10) + nptr[i++] - '0';
 	}
 	return (result * sign);
 }
