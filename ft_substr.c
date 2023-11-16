e13ba0c8679de743161bffb99ca2f030001bd129
pdlmn <jseldardw@gmail.com>
2023-11-16 17:06:22 +0100

    feat: made ft_substr function calculate the size of the returned string correctly

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,16 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:43:12 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/16 14:06:22 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/16 17:02:38 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 // added len check
+// hola\0 4
+// ..la\0 2
+//        3
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
@@ -27,8 +30,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		str = ft_calloc(1, sizeof(char));
 		return (str);
 	}
-	if (len > l)
-		len = l;
+	if (len + start > l)
+		len = l - start;
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
