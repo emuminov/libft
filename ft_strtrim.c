fee87398f3e862b1fd2db53e72c5f234a2319030
pdlmn <jseldardw@gmail.com>
2023-11-16 14:19:36 +0100

    feat: added null guard for ft_strtrim function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:54:30 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/15 12:01:51 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/16 14:19:15 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	char	*str;
 
+	if (!s1 || !set)
+		return (0);
 	start = 0;
 	end = ft_strlen(s1);
 	while (s1[start] && ft_char_in_str(set, s1[start]))
