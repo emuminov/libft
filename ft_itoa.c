7970bdbce521c24dc0f628af775d5ede6708ddda
pdlmn <jseldardw@gmail.com>
2023-11-20 15:54:24 +0100

    refactor: made ft_itoa function leaner

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 02:17:33 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/15 01:04:49 by emuminov         ###   ########.fr       */
+/*   Updated: 2023/11/20 15:53:15 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,61 +19,35 @@ static int	ft_count_digits(int n)
 	int		d;
 
 	nbr = n;
-	if (n == 0)
-		return (1);
-	if (n < 0)
-		nbr = (long) -nbr;
-	else
-		nbr = (long) nbr;
-	d = 0;
+	if (nbr < 0)
+		nbr = -nbr;
+	d = (nbr == 0);
 	while (nbr > 0)
 	{
-		nbr = nbr / 10;
+		nbr /= 10;
 		d++;
 	}
 	return (d);
 }
 
-static int	ft_get_base(long n)
-{
-	long long	div;
-
-	div = 1;
-	while (div * 10 <= n)
-		div *= 10;
-	return (div);
-}
-
-static void	ft_fill(long *nbr, int *base, int *index, char *str)
-{
-	str[*index] = *nbr / *base + '0';
-	*nbr %= *base;
-	*base /= 10;
-	*index += 1;
-}
-
 char	*ft_itoa(int n)
 {
 	int		d;
-	int		i;
-	int		base;
 	long	nbr;
-	char	*str;
+	char	str[12];
 
 	d = ft_count_digits(n);
-	str = malloc(sizeof(char) * (d + (n < 0) + 1));
-	if (!str)
-		return (0);
-	i = 0;
 	nbr = (long) n;
 	if (n < 0)
 	{
-		str[i++] = '-';
+		str[0] = '-';
 		nbr = -nbr;
 	}
-	base = ft_get_base(nbr);
-	while (d-- > 0)
-		ft_fill(&nbr, &base, &i, str);
-	str[i] = '\0';
-	return (str);
+	str[d + (n < 0)] = '\0';
+	while (d--)
+	{
+		str[d + (n < 0)] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	return (ft_strdup(str));
 }
