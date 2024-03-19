c3358ffcf025d23c62fcbf8e6b65a846a6a1bb7b
Eldar Muminov <emuminov@z4r3p7.42lyon.fr>
2024-03-19 16:58:16 +0100

    feat: added overflow check for ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,17 +6,20 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:19:05 by emuminov          #+#    #+#             */
-/*   Updated: 2023/11/21 22:42:40 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:56:05 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 	size_t	t;
 
+	if ((nmemb > 0) && (SIZE_MAX / nmemb > size))
+		return (0);
 	t = nmemb * size;
 	ptr = malloc(t);
 	if (!ptr)
